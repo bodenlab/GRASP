1f792a0b5f5fa16ad56d69a8d91e63279c584c38
gabefoley <gabriel.foley@uqconnect.edu.au>
2018-05-22 11:45:29 +1000

    Added invisible space before items in Guide to make links work correctly

--- asrweb/grasp/src/main/java/com/asr/grasp/GraspApplication.java
+++ asrweb/grasp/src/main/java/com/asr/grasp/GraspApplication.java
@@ -38,8 +38,8 @@ import java.util.zip.ZipOutputStream;
 @SessionScope
 public class GraspApplication extends SpringBootServletInitializer {
 
-	final static String sessionPath = "/Users/marnie/Documents/WebSessions/";
-//		final String sessionPath = "/Users/gabefoley/Documents/WebSessions/";
+//	final static String sessionPath = "/Users/marnie/Documents/WebSessions/";
+		final String sessionPath = "/Users/gabefoley/Documents/WebSessions/";
 //	private final static String sessionPath = "/var/www/GRASP/";
 
 	private final static Logger logger = Logger.getLogger(GraspApplication.class.getName());

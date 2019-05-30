8d6da0092a68bf4f4f4f44fa75f1a539250b62b0
Ariane Mora <ariane.mora@uq.net.au>
2019-05-30 14:18:28 +1000

    Added necessary library files.

---
+++ asrweb/grasp/src/main/resources/public/js/download.js
@@ -0,0 +1,65 @@
+/**
+ * The file that facilitates downloading any of the data from GRASP.
+ **/
+
+let downloadData = function(btnId) {
+  if (btnId === 'download-joint') {
+    downloadGet('/download-ancs', 'joint');
+  } else if (btnId === 'download-tree') {
+    downloadGet('/download-tree');
+  } else if (btnId === 'download-marginal') {
+    downloadGet('/download-ancs', 'marginal');
+  }
+};
+
+let downloadGet = function(url, dataPost) {
+  document.getElementById("download-error-div").style.display = "none";
+  let req = {
+    url: url,
+    type: 'GET',
+    contentType: "application/json",
+    dataType: 'json',
+    success: function (data) {
+      if (data.error !== undefined) {
+        window.alert(data.error);
+      } else {
+        // Now we want to download the data as a file.
+        let filename = data.filename;
+        let fileType = data.filetype;
+        let fileContent = data.filecontent;
+        if (fileContent === "") {
+          if (dataPost === "marginal") {
+            let err = "No marginal reconstructions have been saved, please save some marginal reconstructions and try again :)";
+            document.getElementById("download-error").innerText = err;
+            document.getElementById("download-error-div").style.display = "block";
+
+            window.alert(err);
+          } else {
+            let err = "Looks like you need to save your reconstruction, click the save button in the top left corner :)";
+            document.getElementById("download-error").innerText = err;
+            document.getElementById("download-error-div").style.display = "block";
+
+            window.alert(err);
+            return;
+          }
+        } else {
+          download(fileContent, filename, fileType);
+          document.getElementById("download-error-div").style.display = "none";
+        }
+
+      }
+    }, error: function (err) {
+      console.log(err);
+      document.getElementById("download-error").innerText = "Error downloading, check whether your popups are blocked.";
+      document.getElementById("download-error-div").style.display = "block";
+      window.alert("Error downloading, check if you have popups blocked.");
+    }
+  };
+  if (dataPost !== undefined) {
+    req.data = dataPost;
+    req.type = "POST";
+  }
+
+  $.ajax(req)
+
+};

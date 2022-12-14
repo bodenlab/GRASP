c66c3e96144f5483e34ea285c1c0003246cb34ed
Gabe Foley <gabriel.foley@uqconnect.edu.au>
2022-12-14 11:48:19 +1000

    Add in an example TestPropertiesOverride file

---
+++ src/test/java/com/asr/grasp/TestPropertiesExample.java
@@ -0,0 +1,16 @@
+//package com.asr.grasp;
+//
+//public class TestPropertiesOverride {
+//    /**
+//     * Copy this file to another file called TestPropertiesOverride.
+//     * You'll need to add in your own session path.
+//     *
+//     * Also any variables unique to your test environment should be added to
+//     * the override file.
+//     */
+//
+//    public static final String testFilePath =  "/<path_to_repo>/src/main/resources/data/test/";
+//
+//
+//
+//}
